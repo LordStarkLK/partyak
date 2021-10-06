af111980f10f695b8b8a21d9a4935180263a45a1
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2021-10-06 19:18:40 +0530

    1

--- app/views/homeView.php
+++ app/views/homeView.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="#">
     <title>Home</title>
 
-    <?php linkCSS("home"); ?>
+    <?php linkCSS("home-new"); ?>
     <?php linkCSS("navigation"); ?>
 </head>
 
@@ -17,37 +17,62 @@
     <header>
         <?php linkPhp("navigation") ?>
     </header>
+    <<<<<<< ours=======>>>>>>> theirs
 
 
-
-    <div class="container">
-
-        <div class="left">
-            <div class="test">
-                <label>Life is an event.<br>Make it memorable.</label><br><br>
-                <div class="button">
-                    <input type="button" value="Search Vendor">
+        <div class="container section1">
+            <div class="left column">
+                <div class="content-left">
+                    <h1 class="quote">
+                        Life is an event.<br>
+                        Make it memorable.
+                    </h1>
+                    <button class="search-vendor">Search Vendors</button>
                 </div>
-            </div>
-            <!--<div class="scroll">
-                    <input type="button" value="Scroll">
-                 </div>-->
-        </div>
-        <div class="right">
-            <div class="glass">
-                <div class="box">
-                    <lable><br>cake bakery<br><br></lable>
-                    <img <?php srcIMG("caek.png") ?> alt="Cake Vector">
-                </div>
-                <lable>
-                    <br>We are a bake-house offering<br> services for custom bake orders<br> including cakes etc.We accpet all<br> the kinds of events including<br> weddings, birthday parties and<br> cooperate events. Send a request<br> for reservations.<br><br>
-                </lable>
-                <img <?php srcIMG("Group 6.png") ?> alt="Request Vector">
-            </div>
-        </div>
-    </div>
 
-    <?php linkPhp("footer") ?>
+            </div>
+            <div class="right column">
+                <img class="home-blob" <?php srcIMG("home-blob.png") ?> alt="home blob">
+                <div class="glass glass-phone">
+                    <div class="hamburger-menu">
+                        <img class="hamburger-png" <?php srcIMG("hamburger-menu.png") ?>>
+                    </div>
+                    <div class="glass inner-glass">
+                        <img class="cake-icon" <?php srcIMG("caek.png") ?> alt="cake icon">
+                    </div>
+                    <div class="descripton"></div>
+                    <div class="button request"></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="container section2">
+            <div class="ads-logo"><img class="ads-logo-img" <?php srcIMG("kingbury-ad-logo.png") ?> alt="logo"></div>
+            <div class="ad-details">
+                <img class="ad-details-img" <?php srcIMG("ad-details.png") ?>>
+            </div>
+            <div class="ad-imgs"><img class="ad-imgs-cards" <?php srcIMG("visacards.png") ?>></div>
+        </div>
+        <div class="container section3">
+            <div class="column best-seller-text">
+                <h2 class="best-seller-h2">Best<br>Seller</h2>
+            </div>
+            <div class="column best-seller-icons">
+                <table class="best-seller-table">
+                    <tr>
+                        <th>Company</th>
+                        <th>Contact</th>
+                        <th>Country</th>
+                    </tr>
+                    <tr>
+                        <th>Company</th>
+                        <th>Contact</th>
+                        <th>Country</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
 
 </body>
 
