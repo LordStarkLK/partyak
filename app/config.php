e43dd70ac7c28fe6e1399cb1d1fbdd73c6656b4d
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2021-10-10 01:31:05 +0700

    This commit for config.php

--- app/config.php
+++ app/config.php
@@ -1,4 +1,8 @@
 <?php
 
+define("HOST", "193.168.192.2");
+define("USER", "u117929562_dhp2");
+define("DATABASE", "u117929562_Partyak");
+define("PASSWORD", "Partyak#2021");
 
 define("BASEURL", "http://localhost/partyak");
