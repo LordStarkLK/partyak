b3ecf4b6f25fad0c79ed8ac9af77b9ef3fd57997
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2021-10-11 13:29:05 +0700

    This commit for view of customer payment details

---
+++ app/views/customer/customerProfilePaymentDView.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+ 
+    <?php linkCSS("customer/customerProfilePaymentD"); ?>
+    <?php linkCSS("customerNavigation"); ?>
+    <?php linkCSS("footer");?>
+    
+   
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <!-- <link rel="stylesheet" href="Customer.browse.SearchVendor.css"> -->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <header>
+    <?php linkPhp("customerNavigation") ?>
+  </header>
+  
+  <div class="container">
+        <div class="custopic">
+            <p>My  Payment  Details</p>
+        </div>
+
+
+    <div class="cusevent">
+    <table>
+      <div class="cusraw1">
+        <tr>
+          <th>Payment No</th>
+          <th>Vendor</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th>Status</th>
+          <th>Paid Amount</th>
+          <th>Total Amount</th>
+        </tr>
+      </div>
+        <tr>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          
+        </tr>
+        <tr>
+          <td>            </td>
+          <td>            </td>
+          <td>            </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+        </tr>
+        <tr>
+          <td>            </td>
+          <td>            </td>
+          <td>            </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+        </tr>
+        <tr>
+          <td>            </td>
+          <td>            </td>
+          <td>            </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+        </tr>
+        <tr>
+          <td>            </td>
+          <td>            </td>
+          <td>            </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+         
+        </tr>
+        <tr>
+          <td>           </td>
+          <td>           </td>
+          <td>           </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+        </tr>
+        <tr>
+          <td>           </td>
+          <td>           </td>
+          <td>           </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+          <td>          </td>
+        </tr>
+      </table>
+    </div>
+     
+   </div>
+
+   <?php linkPhp("footer") ?>
+    
+</body>
+</html>
