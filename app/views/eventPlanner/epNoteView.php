8ea2fbfb6d084991eceafa7e25b16082344824a4
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2021-10-15 18:37:33 +0530

    event planner sidebar completely done. payment page structure is done css is not completed

--- app/views/eventPlanner/epNoteView.php
+++ app/views/eventPlanner/epNoteView.php
@@ -21,38 +21,38 @@
 
             <ul class="sidenav-links">
                 <li>
-                    <a href="<?php echo BASEURL . '/spAnalytics'; ?>">
+                    <a href="<?php echo BASEURL . '/epAnalytics'; ?>">
                         <i class="fa fa-pie-chart" aria-hidden="true"></i>
                         <span class="links_name">Analytics</span>
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo BASEURL . '/spProfile'; ?>">
+                    <a href="<?php echo BASEURL . '/epProfile'; ?>">
                         <i class="fa fa-user-circle-o" aria-hidden="true"></i>
                         <span class="links_name">Profile</span>
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo BASEURL . '/spService'; ?>">
+                    <a href="<?php echo BASEURL . '/epService'; ?>">
                         <i class="fa fa-server" aria-hidden="true"></i>
                         <span class="links_name">Services</span>
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo BASEURL . '/spBooking'; ?>">
+                    <a href="<?php echo BASEURL . '/epBooking'; ?>">
                         <i class="fa fa-list" aria-hidden="true"></i>
                         <span class="links_name">Booking</span>
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo BASEURL . '/spMarketing'; ?>">
+                    <a href="<?php echo BASEURL . '/epMarketing'; ?>">
                         <i class="fa fa-sticky-note" aria-hidden="true"></i>
                         <span class="links_name">Marketing</span>
                     </a>
                 </li>
 
                 <li>
-                    <a href="<?php echo BASEURL . '/spWallet'; ?>">
+                    <a href="<?php echo BASEURL . '/epWallet'; ?>">
                         <i class="fa fa-usd" aria-hidden="true"></i>
                         <span class="links_name">Wallet</span>
                     </a>
@@ -70,7 +70,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="<?php echo BASEURL . '/spReview '; ?>">
+                    <a href="<?php echo BASEURL . '/epReview '; ?>">
                         <i class="fa fa-star" aria-hidden="true"></i>
                         <span class="links_name">Review</span>
                     </a>
