36a4576915be01195dca194455a157a789b3b266
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2021-10-24 02:23:14 +0700

    This commit for vendor service review

---
+++ public/js/vendor/vendorReview.js
@@ -0,0 +1,8 @@
+var review =document.getElementById("rbo");
+
+var rebtn = document.getElementById("rbtn");
+
+reBtn.onclick = function(){
+    review.style.display="flex";
+    rebtn.style.display="none";
+}
