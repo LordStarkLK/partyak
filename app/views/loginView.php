78d2e6afd029f5d90cc499de6c33f433c04c142a
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2021-10-23 21:05:39 +0530

    googlle login removed

--- app/views/loginView.php
+++ app/views/loginView.php
@@ -54,30 +54,14 @@
                     <p class="account">Not a member? <a href="<?php echo BASEURL . '/home/signup'; ?>"> Register</a></p>
                 </div>
 
-                <div class="sociallogin">
+                <!-- <div class="sociallogin">
                     <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
     <?php linkJS("loginValidation") ?>
     <script src="https://kit.fontawesome.com/3220c9480a.js" crossorigin="anonymous"></script>
-    <script>
-        function onSignIn(googleUser) {
-            // Useful data for your client-side scripts:
-            var profile = googleUser.getBasicProfile();
-            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log("Image URL: " + profile.getImageUrl());
-            console.log("Email: " + profile.getEmail());
-
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token);
-        }
-    </script>
 
 
 </body>
