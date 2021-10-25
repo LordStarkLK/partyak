eff2e767136dada11059c9439ea0488000908d79
ADHDilsithmi <84245681+ADHDilsithmi@users.noreply.github.com>
2021-10-25 20:08:44 +0530

    link service provider calendar

--- app/views/vendor/spReviewVIew.php
+++ app/views/vendor/spReviewVIew.php
@@ -57,7 +57,7 @@
           </a>
         </li>
         <li>
-          <a href="#">
+        <a href="<?php echo BASEURL . '/spChat'; ?>">
             <i class="fa fa-envelope" aria-hidden="true"></i>
             <span class="links_name">Chat</span>
           </a>
