8c2f658d16f918d85dddec7b8174369e51d31225
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2021-11-30 02:17:55 +0700

    This commit for do some changes of my event

--- app/models/customerMyEventModel.php
+++ app/models/customerMyEventModel.php
@@ -2,7 +2,7 @@
 class CustomerMyEventModel extends database
 {
     public function getEventDetails($id){
-        $query = "SELECT event_type, event_name, start_date, end_date, location, noOfGuest, plan_type  FROM `on_your_own_planning` WHERE user_id='$id' ";
+        $query = "SELECT event_type, event_name, start_date, end_date, location, noOfGuest, plan_type  FROM event WHERE user_id='$id' ";
         $result = mysqli_query($GLOBALS['db'],$query);
         if(mysqli_num_rows($result) >= 0){
             return $result;
