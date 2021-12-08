90b7faedfa5c3d6e42312a38e08d4b28dcc42ece
ADHDilsithmi <84245681+ADHDilsithmi@users.noreply.github.com>
2021-12-09 00:40:25 +0530

    marketing content upload

--- app/controllers/spMarketing.php
+++ app/controllers/spMarketing.php
@@ -6,12 +6,79 @@ class SpMarketing extends FrameworkPartyak
     {
         $this->helper("linker");
         $this->preventBack("vendor");
+
+        //include this to call CRUD functions from the controller files
+        $this->MarketingModel = $this->model("SpMarketingModel");
     }
 
     public function index()
     {
+        $id=$_SESSION['userId'];
+
+        if (isset($_POST['submit'])) {
+            $file = $_FILES['file'];
+                // print_r($file);
+            $fileName = $_FILES['file']['name'];
+            $fileTmpName = $_FILES['file']['tmp_name'];
+            $fileSize = $_FILES['file']['size'];
+            $fileError = $_FILES['file']['error'];
+            $fileType = $_FILES['file']['type'];
+    
+            $fileExt = explode('.' , $fileName);
+            //getting the file type 
+            $fileActualExt = strtolower(end($fileExt)); 
+    
+            $allowed = array('jpg' , 'jpeg' , 'png' );
+    
+            //check the file type
+            if(in_array($fileActualExt, $allowed)){
+                //ckeck errors
+                if($fileError === 0){
+                    //check the file size
+                    if ($fileSize < 1000000){
+                        //set a unique name
+                        $fileNameNew = uniqid('', true).".".$fileName ;
+                        //upload to the folder
+                        $fileDestination = '../public/img/marketingContent/'.$fileNameNew;
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        // header("Location: spMarketingView.php?uploadsuccess");
+                        
+                        // $sql = "INSERT INTO upload(name) VALUES('$fileNameNew') ";
+                        // $sql = "INSERT into upload(name) VALUES('$fileNameNew') ";
+
+                        $this->MarketingModel->uploadMarketingContent($id, $fileNameNew);
+            
+                        // if(mysqli_query($conn,$sql)){
+                        //     echo "File uploaded successfully!";
+                        // }
+                        // else{
+                        //     echo "Uploading fail!";
+                        // }
+                        
+                    }
+                    else{
+                        echo "Your file is too big!";
+                    }
+                }
+                else{
+                    echo "Error in uploading the file!";
+                }
+    
+            }
+            else{
+                echo "You cannot upload files of this type";
+            }
+    
+        }
+
+
+
+
+        
+        $data['contents'] = $this->MarketingModel->getUploadedContentDetails($id);
+
         // echo "Hi";
-        $this->view("vendor/spMarketingView");
+        $this->view("vendor/spMarketingView", $data);
     }
 
    
--- app/views/vendor/spMarketingView.php
+++ app/views/vendor/spMarketingView.php
@@ -90,41 +90,156 @@
       <div class="home-content">
         <div class="uploaded_area">
           <a>Uploaded Marketing Content</a>
-          <div class="file_details">
-            <div class="details">
-              <a>2021/10/09</a>
-              <div class="file">
-                <div class="file_icon">
-                  <i class="fa fa-file-image-o fa-3x" aria-hidden="true"></i>
+          
+
+          <?php while ($row = mysqli_fetch_assoc($data['contents'])){
+            $i =1;
+            // $check = "reject";
+            echo "
+              <div class=\"file_details\">
+              <div class=\"details\">
+                <a>$row[upload_date]</a>
+                <div class=\"file\">
+                  <div class=\"file_icon\">
+                    <i class=\"fa fa-file-image-o fa-3x\" aria-hidden=\"true\"></i>
+                  </div>";
+
+                  $nameSplitArray = explode('.', $row['content'],3);
+                  $newName = end($nameSplitArray);
+                  echo "<div class=\"file_name\">
+                    <a>$newName</a>
+                  </div>
+
                 </div>
-                <div class="file_name">
-                  <a>My file.png</a>
+              </div>";
+              
+              
+               if($row['upload_status'] == "accept" ){
+                echo "
+                <div class=\"status accept\">
+                  <a>Accepted</a>
+                  <i class=\"fa fa-check-circle-o fa-lg\" aria-hidden=\"true\" ></i>
                 </div>
-              </div>
-            </div>
-            <div class="status">
-              <a style="color: rgb(75, 224, 88);">Pending</a>
-              <i class="fa fa-pause-circle-o fa-lg" aria-hidden="true" style="color: rgb(75, 224, 88);"></i>
-            </div>
-          </div>
+              </div>";
+              }
+              elseif($row['upload_status'] == "reject"){
+                echo "
+                <div class=\"status reject\">
+                  <a>Rejected</a>
+                  <i class=\"fa fa-times-circle-o fa-lg\" aria-hidden=\"true\" ></i>
+                </div>
+              </div>";
+              }
+              else{
+                echo "
+                <div class=\"status pending\">
+                  <a>Pending</a>
+                  <i class=\"fa fa-pause-circle-o fa-lg\" aria-hidden=\"true\" ></i>
+                </div>
+              </div>";
+              }
+          }
+          ?>
+          
+          <div class="paymentBtn">
           <a href="<?php echo BASEURL . '/payment'; ?>"><button>Pay now</button></a>
+          </div>
+          
         </div>
+
         <div class="submission_area">
           <div class="title"><a>Upload Your File</a></div>
-          <div class="sub_title"><a>(File should be JPG or PNG)</a></div>
-          <div class="submit_file">
+          <div class="sub_title"><a>(File should be JPG, JPEG or PNG)</a></div>
+<!-- here newly added codes -->
+          <div class="upload-container">
+            <div class="wrapper">
+                <div class="image">
+                    <img id="contentImg" src="" alt="">
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <i class="fas fa-cloud-upload-alt"></i>
+                    </div>
+                    <div class="text">
+                        No file chosen, yet!
+                    </div>
+                </div>
+                <div id="cancel-btn">
+                    <i class="fas fa-times"></i>
+                </div>
+                <div class="file-name">
+                    File name here
+                </div>
+            </div>
+            
+            <button type="file" name="file" onclick="defaultBtnActive()" id="custom-btn">Choose a file</button>
+            
+            <form action="<?php echo BASEURL . '/spMarketing/index'; ?>" method="POST" enctype="multipart/form-data">
+            
+            <input id="default-btn" type="file" name="file" hidden>
+            <button type="submit" name="submit">UPLOAD </button>
+            </form>
+          </div>
+
+<!-- end -->
+          <!-- <div class="submit_file">
             <i class="fa fa-upload fa-4x" aria-hidden="true"></i>
             <br><a>Drag & Drop</a>
             <br><a>or</a>
             <br><a>Browse File to Upload</a>
           </div>
-          <button tupe="submit">Upload</button>
+          <button tupe="submit">Upload</button> -->
+
+
+
         </div>
       </div>
     </section>
   </div>
   <?php linkJS("vendor/spMarketing"); ?>
   <?php linkPhp("footer") ?>
+
+<script>
+  const wrapper = document.querySelector(".wrapper");
+const fileName = document.querySelector(".file-name");
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const cancelBtn = document.querySelector("#cancel-btn i");
+// const img = document.querySelector("img");
+const img = document.getElementById("contentImg")
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+
+function defaultBtnActive(){
+    defaultBtn.click();
+}
+
+defaultBtn.addEventListener("change", function(){
+    const file = this.files[0];
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function(){
+            const result = reader.result;
+            img.src = result;
+            wrapper.classList.add("active");
+        }
+        cancelBtn.addEventListener("click", function(){
+            img.src = "";
+            wrapper.classList.remove("active");
+        })
+        //read the content of the file
+        reader.readAsDataURL(file);
+    }
+
+    if(this.value){
+        let valueStore = this.value.match(regExp);
+        fileName.textContent = valueStore;
+    }
+
+});
+</script>
+
+
+
 </body>
 
 </html>
