36f4a971ab4d9d0570a0ec643bb909b7073e68c7
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2022-01-08 13:00:03 +0530

    review-system resolving conflicts

--- app/controllers/spService.php
+++ app/controllers/spService.php
@@ -15,17 +15,27 @@ class SpService extends FrameworkPartyak
     public function index($service_id)
     {
 
-        $id=$_SESSION['userId'];
+        $id = $_SESSION['userId'];
         // $serName = 'Avendra';
 
         $data['service'] = $this->ServiceModel->getServiceInfo($service_id);
         $data['package_data'] = $this->ServiceModel->getPackageInfo($service_id);
 
+        $errors = array();
+        $errors["ratedStars"] = "";
+        $errors["review"] = "";
 
-        
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $customId = $_POST["review"];
+            $customName = $_POST["user_id"];
+            $eventName = $_POST["ratedIndex"];
+            $eventDate = $_POST["event_date"];
+        }
+>>>>>>> 6f60ca3 (review-system resolving conflicts)
+
+
         // echo "Hi";
         $this->view("vendor/spServiceView", $data);
     }
-
-   
 }
