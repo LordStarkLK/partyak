762c1656303b550e38df4fc414ae3698d41d3666
Dinil Ratnayake <84171952+DinilR@users.noreply.github.com>
2022-01-10 16:19:51 +0530

    commit for removing search for marketing

--- app/views/admin/adminMarketingView.php
+++ app/views/admin/adminMarketingView.php
@@ -41,16 +41,6 @@
 
                     </ul>
 
-                    <div class="search-container">
-                        <form action="/action_page.php">
-                            <input type="text" placeholder="Search Marketing.." name="search">
-
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                            <!-- <i class="fas fa-search"></i> -->
-
-
-                        </form>
-                    </div>
 
 
                 </div>
