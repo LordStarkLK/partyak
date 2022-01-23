3eea85205f770674e6f5080be794d5ff6d66516f
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2022-01-24 01:37:11 +0530

    reversed

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ session_start();
 
 
 // Database configuration file
-include "http://www.lankanphoneprice.com/../app/config.php";
+include "../app/config.php";
 
 // Include autoloader and routing file
-include "http://www.lankanphoneprice.com/../core/init.php";
+include "../core/init.php";
