8d64c962f1ff75b01afa5ca3fb163dc5f404069c
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2022-01-24 05:38:50 +0530

    done

--- app/controllers/epEventAdd.php
+++ app/controllers/epEventAdd.php
@@ -32,7 +32,6 @@ class epEventAdd extends FrameworkPartyak
             $eventType = $_POST["event_type"];
             $date = $_POST["date"];
             $noOfGuests = $_POST["no_of_guests"];
-            $img1 = $_POST["img1"];
             $location = $_POST["location"];
             $description = $_POST["description"];
 
@@ -40,7 +39,6 @@ class epEventAdd extends FrameworkPartyak
             if (empty($eventType)) $errors["event_type"] = "Event Type is required";
             if (empty($date)) $errors["date"] = "Date is required";
             if (empty($noOfGuests)) $errors["no_of_guests"] = "No of Guests is required";
-            if (empty($img1)) $errors["img1"] = "Atleast 1 image is required";
             if (empty($location)) $errors["location"] = "Location is required";
             if (empty($description)) $errors["description"] = "Description is required";
 
@@ -56,12 +54,29 @@ class epEventAdd extends FrameworkPartyak
 
             if ($numberOfErrors == 0) {
                 //Insert data
-                $this->epAddEvent->addEvent($userId,$eventType,$date,$noOfGuests, $img1,$location,$description);
+                if (!empty($_FILES["img1"]["name"])){
+
+                //Process the new image that is uploaded by the user
+                $target_dir = "../public/img/event-planner/event-imgs/";
+                $target_file = $target_dir . basename($_FILES["img1"]["name"]);
+                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                $filename = $_FILES["img1"]["name"];
+
+                move_uploaded_file($_FILES["img1"]["tmp_name"], $target_file);
+
+                $timestamp = time();
+                $image = $userId . $timestamp . "." . $imageFileType; //generating an unique name to the image file
+                rename("../public/img/event-planner/event-imgs/$filename", "../public/img/event-planner/event-imgs/$image"); //adding the generated name to the file
+                
+                $this->epAddEvent->addEvent($userId,$eventType,$date,$noOfGuests,$image,$location,$description);
                 $this->view("eventPlanner/epEventAddView");
+                }else{
+                    $errors["img1"] = "Image is required";
+                    $data["errors"] = $errors;
+                    $this->view("eventPlanner/epEventAddView", $data);
+                }
             }
         }
-
-
         $data["errors"] = $errors;
         $this->view("eventPlanner/epEventAddView", $data);
     }
