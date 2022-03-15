22f0a13d3ef834dbb60fd6742cf899cac02702df
DinilR <dinilsratnayake@gmail.com>
2022-03-15 15:00:57 +0530

    commit for adding how it works page for admin

---
+++ app/controllers/adminHowItWorks.php
@@ -0,0 +1,33 @@
+<?php 
+
+class AdminHowItWorks extends FrameworkPartyak{
+    public function __construct(){
+        $this->helper("linker");
+        $this->user= $this->model('adminTandCModel');
+        $this->preventBack("admin");
+    }
+    
+    public function index(){
+
+        // IF($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST["id"] != "insert"){
+        //     $title = $_POST["title"];
+        //     $content = $_POST["content"];
+        //     $id = $_POST["id"];
+            
+        //     $this->user->updateTandC($title,$content,$id);
+        // }
+
+        // IF($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST["id"] == "insert"){
+        //     $title = $_POST["title"];
+        //     $content = $_POST["content"];
+        //     // $id = $_POST["id"];
+            
+        //     $this->user->addTandC($title,$content);
+        // }
+        // $data['tandc'] = $this->user->getTandC();
+
+        $this->view("admin/adminHowItWorksView");
+    }
+}
+
+?>
