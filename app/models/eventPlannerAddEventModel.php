2e0aafb30f40e090f8cff1885c8fd759a9074028
LordStarkLK <53448703+LordStarkLK@users.noreply.github.com>
2022-03-18 18:54:46 +0530

    small change

--- app/models/eventPlannerAddEventModel.php
+++ app/models/eventPlannerAddEventModel.php
@@ -3,7 +3,6 @@ class eventPlannerAddEventModel extends database
 {
     public function addEvent($userId,$eventType,$date,$noOfGuests, $image,$location,$description)
     {
-        //Insert data to child tables of user - on_your_own_planning table
         $query = "INSERT INTO `event_planner_event_pages`(`user_id`, `event_type`, `date`, `no_of_guests`, `image1`, `location`, `description`) VALUES
          ('$userId','$eventType','$date','$noOfGuests','$image','$location','$description');";
         mysqli_query($GLOBALS['db'], $query);
