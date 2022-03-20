29bb1d4f7be1155d5e6fdb722f2ecf59131b9a71
DinilR <dinilsratnayake@gmail.com>
2022-03-21 00:15:27 +0530

    commit for display notifications from backend(except mark as read)

--- app/models/notificationModel.php
+++ app/models/notificationModel.php
@@ -15,8 +15,32 @@ class NotificationModel extends Database{
             $query = "SELECT * FROM notifications WHERE user_id = '$userId' AND DATE(date)<>DATE(NOW()) ORDER BY date DESC";
         }
 
-        $results = mysqli_query(GLOBALS["db"],$query);
+        $results = mysqli_query($GLOBALS["db"],$query);
 
         return $results;
     }
+
+    public function getUnreadNotificationDetails($userId, $isToday)
+    {
+        if ($isToday === "1") {
+            $query = "SELECT * FROM notifications WHERE user_id = '$userId' AND DATE(date)=DATE(NOW()) AND notification_status='0' ORDER BY date DESC";
+        } else if ($isToday === "0") {
+            $query = "SELECT * FROM notifications WHERE user_id = '$userId' AND DATE(date)<>DATE(NOW()) AND notification_status='0' ORDER BY date DESC";
+        }
+
+        $results = mysqli_query($GLOBALS["db"], $query);
+
+        return $results;
+    }
+
+    
+    public function updateUserNotification($userId, $notificationId)
+    {
+        $query = "UPDATE notifications SET notification_status='1' WHERE user_id='$userId' AND notification_id='$notificationId'";
+
+        mysqli_query($GLOBALS["db"], $query);
+    }
+
+
+
 }
