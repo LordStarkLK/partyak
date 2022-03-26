938d9fcfb9c3d5e0c163419f5c60440f28ed90d3
Dilsithmi Abeywickrama <2019cs032@stu.ucsc.lk>
2022-03-26 20:19:49 +0530

    changed

--- app/controllers/spPackage.php
+++ app/controllers/spPackage.php
@@ -17,6 +17,7 @@ class SpPackage extends FrameworkPartyak
         //initilaize these varibles for server 
 
         $id=$_SESSION['userId'];
+        $data['service_id'] = $service_id;
 
         $errors = array();
         $errors["serviceName"] = "";
@@ -28,14 +29,13 @@ class SpPackage extends FrameworkPartyak
         $errors["packageUnitprice"] = "";
         // $errors["noofguest"] = "";
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             //Get data from the form submission
             $serviceName = $_POST["serviceName"];
             $packageName = $_POST["packageName"];
             $packageDescription = $_POST["packageDescription"];
             $packageValidFrom = $_POST["packageValidFrom"];
-            // $city = $_POST["city"];
             $packageValidTo = $_POST["packageValidTo"];
             $packageFixedprice = $_POST["packageFixedprice"];
             $packageUnitprice = $_POST["packageUnitprice"];
@@ -46,7 +46,6 @@ class SpPackage extends FrameworkPartyak
             if (empty($packageName)) $errors["packageName"] = "Package Name is required";
             if (empty($packageDescription)) $errors["packageDescription"] = "Package Description is required";
             if (empty($packageValidFrom)) $errors["packageValidFrom"] = "Package Validation Date is required";
-            // if ($city == "null") $errors["city"] ="Location is required";
             if (empty($packageValidTo)) $errors["packageValidTo"] = "Package Validation Date is required";
 
 
@@ -58,15 +57,18 @@ class SpPackage extends FrameworkPartyak
 
                 if ($value != "") {
                     $numberOfErrors++;
+                    echo "Hi";
                 }
             }
 
             if ($numberOfErrors == 0) {
                 //Insert data
+                
                 $this->PackageModel->addPackageDetail($serviceName, $packageName, $packageDescription, $packageValidFrom, $packageValidTo, $service_id, $packageUnitprice, $packageFixedprice);
             }
         }
-            $data['service_id'] = $service_id;
+            
+            
             $data["errors"] = $errors;
             $data['packages'] = $this->PackageModel->getPackageDetails($service_id);
             $data['serName'] = $this->PackageModel->getReleventServiceName($service_id);
@@ -74,5 +76,75 @@ class SpPackage extends FrameworkPartyak
             $this->view("vendor/spPackageView" , $data);
 
     }
+
+    public function updatePackage($package_id){
+
+        
+        
+
+        $errors = array();
+        $errors["serviceName"] = "";
+        $errors["packageName"] = "";
+        $errors["packageDescription"] = "";
+        $errors["packageValidFrom"] = "";
+        $errors["packageValidTo"] = "";
+        $errors["packageFixedprice"] = "";
+        $errors["packageUnitprice"] = "";
+        // $errors["noofguest"] = "";
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            //Get data from the form submission
+            $serviceName = $_POST["serviceName"];
+            $packageName = $_POST["packageName"];
+            $packageDescription = $_POST["packageDescription"];
+            $packageValidFrom = $_POST["packageValidFrom"];
+            $packageValidTo = $_POST["packageValidTo"];
+            $packageFixedprice = $_POST["packageFixedprice"];
+            $packageUnitprice = $_POST["packageUnitprice"];
+
+
+            //Empty check
+            if (empty($serviceName)) $errors["serviceName"] = "Service Name is required";
+            if (empty($packageName)) $errors["packageName"] = "Package Name is required";
+            if (empty($packageDescription)) $errors["packageDescription"] = "Package Description is required";
+            if (empty($packageValidFrom)) $errors["packageValidFrom"] = "Package Validation Date is required";
+            if (empty($packageValidTo)) $errors["packageValidTo"] = "Package Validation Date is required";
+
+
+
+
+            /* Count number of validation failures */
+            $numberOfErrors = 0;
+            foreach ($errors as $key => $value) {
+
+                if ($value != "") {
+                    $numberOfErrors++;
+                    // echo "Hi";
+                }
+            }
+
+            if ($numberOfErrors == 0) {
+                //Insert data
+                
+                $this->PackageModel->updatePackageDetails($serviceName,$packageName, $packageDescription, $packageValidFrom, $packageValidTo, $package_id, $packageUnitprice, $packageFixedprice);
+            }
+        }
+            
+            
+            $data["errors"] = $errors;
+            
+            // echo "Hi";
+            $this->redirect('spPackage');
+
+    }
+
+    public function deletePackage($package_id,$service_id){
+        $this->PackageModel->deletePackage($package_id);
+        $this->index($service_id);
+    }
+
+    
+
 }
 
--- app/models/spPackageModel.php
+++ app/models/spPackageModel.php
@@ -5,16 +5,20 @@ class SpPackageModel extends Database
     
     public function addPackageDetail($serviceName, $packageName, $packageDescription, $packageValidFrom, $packageValidTo, $service_id, $packageUnitprice, $packageFixedprice)
     {
-        $result = mysqli_fetch_assoc(mysqli_query($GLOBALS['db'], "SELECT $service_id FROM other_service WHERE $service_id='$service_id' LIMIT 1"));
-        $serviceId = $result["$service_id"];
+        // $result = mysqli_fetch_assoc(mysqli_query($GLOBALS['db'], "SELECT $service_id FROM other_service WHERE $service_id='$service_id' LIMIT 1"));
+        // $serviceId = $result["$service_id"];
 
         $query = "INSERT INTO package(service_id, service_name, package_name, description, valid_from, valid_to, per_unit_price, fixed_price) 
-        VALUES ('$serviceId', '$serviceName', '$packageName', '$packageDescription' , '$packageValidFrom', '$packageValidTo' , '$packageUnitprice', '$packageFixedprice')";
-        mysqli_query($GLOBALS['db'], $query);
+        VALUES ('$service_id', '$serviceName', '$packageName', '$packageDescription' , '$packageValidFrom', '$packageValidTo' , '$packageUnitprice', '$packageFixedprice')";
+        $result=mysqli_query($GLOBALS['db'], $query);
+        if($result){
+            echo "Hi";
+        }
+        return $result;
     }
 
     public function getPackageDetails( $service_id){
-        $query = "SELECT service_id, per_unit_price, fixed_price, package_name, valid_from, valid_to FROM package where service_id='$service_id' ";
+        $query = "SELECT * FROM package where service_id='$service_id' ";
         $result = mysqli_query($GLOBALS['db'],$query);
         if(mysqli_num_rows($result) >= 0){
             return $result;
@@ -29,5 +33,16 @@ class SpPackageModel extends Database
         } 
     }
 
+    public function updatePackageDetails($serviceName,$packageName, $packageDescription, $packageValidFrom, $packageValidTo, $package_id, $packageUnitprice, $packageFixedprice)
+    {
+        $query = "UPDATE package SET package_name='$packageName', description='$packageDescription', valid_from='$packageValidFrom', valid_to='$packageValidTo', per_unit_price='$packageUnitprice', fixed_price='$packageFixedprice' WHERE package_id='$package_id' ";
+        mysqli_query($GLOBALS['db'], $query);
+    }
+
+    public function deletePackage($package_id){
+        $query = "DELETE FROM package WHERE package_id=$package_id";
+        $result = mysqli_query($GLOBALS["db"], $query);
+        return $result;
+    }
 
 }
